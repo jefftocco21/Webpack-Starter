3a54f7c65833042d27784fb714c856d5d3b2f9cd
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-06 11:13:44 -0400

    Hashed filename

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name][contenthash].js',
     },
     module: {
         rules:[
