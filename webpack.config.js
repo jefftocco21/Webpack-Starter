0820d9b6103e1393ed92b8816fc944e18f047fd6
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-06 10:36:34 -0400

    Create a template.html file to be built

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
     plugins:[
         new HtmlWebpackPlugin({
             title: 'Webpack App',
-            filename: 'index.html'
+            filename: 'index.html',
+            template: 'src/template.html'
         })
     ]
 }
