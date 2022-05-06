b011c496c5c329ae28473d9054bd2e63d0bbad49
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-06 12:55:57 -0400

    adding source map dev tool to config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: {
        bundle: path.resolve(__dirname, 'src/index.js')
     },
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
