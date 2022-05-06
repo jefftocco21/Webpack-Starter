b622e1712c603ec056c6823aeedcd0a3f2f2aadc
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-06 12:54:02 -0400

    Adding clean to config prevents multiple bundle files

--- dist/bundle6eb1f3fb51d46163bae6.js
+++ dist/bundle6eb1f3fb51d46163bae6.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _joke_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./joke.js */ \"./src/joke.js\");\n/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/main.scss */ \"./src/styles/main.scss\");\n\r\n\r\n\r\nconsole.log((0,_joke_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])());\n\n//# sourceURL=webpack://webpack-starter/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _joke_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./joke.js */ \"./src/joke.js\");\n/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/main.scss */ \"./src/styles/main.scss\");\n\r\n\r\n\r\nconsole.log((0,_joke_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])());\r\nconsole.log(1);\n\n//# sourceURL=webpack://webpack-starter/./src/index.js?");
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import createJoke from './joke.js';
 import './styles/main.scss';
 
 console.log(createJoke());
+console.log(1);
